19afdfc2843d2213ba6066262c162cde748160a1
Philipp Leon <phil897414@gmail.com>
2025-11-10 16:41:58 +0100

    Api Clients

---
+++ SpooltrackingAPI.Client/api/api.ts
@@ -0,0 +1,5 @@
+export * from './spool.service';
+import { SpoolService } from './spool.service';
+export * from './spoolBrand.service';
+import { SpoolBrandService } from './spoolBrand.service';
+export const APIS = [SpoolService, SpoolBrandService];
---
+++ SpooltrackingAPI.Client/model/models.ts
@@ -0,0 +1,7 @@
+export * from './createSpoolBrandModel';
+export * from './createSpoolModel';
+export * from './problemDetails';
+export * from './spool';
+export * from './spoolBrand';
+export * from './updateSpoolBrandModel';
+export * from './updateSpoolModel';
